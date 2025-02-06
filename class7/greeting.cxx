de9b3627306303e3cb208a9384d44734ea0dcae1
Kai Germaschewski <kai.germaschewski@gmail.com>
2025-02-06 09:51:38 -0500

    class7: starter code

---
+++ class7/greeting.cxx
@@ -0,0 +1,9 @@
+
+#include "hello.h"
+
+#include <iostream>
+
+void greeting(std::string name)
+{
+  std::cout << "Hi there, " << name << "\n";
+}
---
+++ class7/hello.cxx
@@ -0,0 +1,11 @@
+
+#include "hello.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  greeting("everyone");
+
+  std::cout << "10 factorial is " << factorial(10) << "\n";
+}
